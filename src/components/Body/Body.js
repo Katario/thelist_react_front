6d14c061e30546169a9009c024fbcdea6e1381a8
Katario <boe.hadrien@gmail.com>
2020-06-23 11:35:47 +0200

    Added axios and first call to api in navbar

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import api from '../../api/api';
 import NavBar from '../NavBar/NavBar';
 import Content from '../Content/Content';
 import './Body.css';
 
-let Body = () => {
-  return (
-    <div className="Body container">
-      <NavBar />
-      <Content />
-    </div>
-  );
-}
+export default class Body extends React.Component {
+  
+  constructor(props) {
+    super(props);
 
-export default Body;
+    this.state = {
+      lists: []
+    }  
+  }
+
+  componentDidMount() {
+    api.get('/list')
+      .then(res => {
+        const lists = res.data.lists;
+        this.setState({ lists });
+      })
+  }
+  
+  render() {
+    return (
+      <div className="Body container">
+        <NavBar lists={this.state.lists} />
+        <Content />
+      </div>
+    );
+  }
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,24 +2,27 @@ import React from 'react';
 import Item from '../Item/Item';
 import './NavBar.css';
 
-let itemList = () => {
-  let list = [];
-  for (let i = 0; i < 16; i++) {
-    list.push(<Item key={i} />);
+export default class NavBar extends React.Component {
+
+ 
+  pushItemList = () => {
+    return this.props.lists.map((list, i) => {
+      return <Item key={i} title={list.title} />;
+    })
   }
+ 
+  render() {
+    const isList = this.props.lists.length > 0 ? true : false;
 
-  return list;
-}
-
-let NavBar = () => {
-  return (
-    <div className="NavBar">
-      <div className="NavBar-content">
-        This is the navBar Space.
-        {itemList()}
+    return (
+      <div className="NavBar">
+        <div className="NavBar-content">
+          This is the navBar Space.
+          {isList &&
+           this.pushItemList()
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default NavBar;
